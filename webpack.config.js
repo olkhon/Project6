84dddf10861929b36db1d1ec2413fc19942f9182
olkhon <alexander.stapel@gmail.com>
2020-01-15 12:58:22 +0300

    plugin html-webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const webpack = require('webpack')
+const HtmlWebPackPlugin = require('html-webpack-plugin')
 
 module.exports = {
     entry: './src/client/index.js',
@@ -9,5 +10,11 @@ module.exports = {
             exclude: /node_modules/,
             loader: "babel-loader"
         }]
-    }
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: "./src/client/views/index.html",
+            filename: "./index.html"
+        })
+    ]
 }
