79f50754beb66842582805cb9cd314d2f5471679
olkhon <alexander.stapel@gmail.com>
2020-01-22 22:25:26 +0300

    entry point webpack

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const HtmlWebPackPlugin = require('html-webpack-plugin')
 
 module.exports = {
     mode: "development",
-    entry: path.resolve(__dirname, 'src') + '/client/index.js',
+    entry: './src/client/index.js',
     module: {
         rules: [{
                 test: '/\.js$/',
