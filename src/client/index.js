ab327e2987f9fcd9e389332ee4bb369b8da4cc4b
olkhon <alexander.stapel@gmail.com>
2020-02-02 00:52:16 +0300

    update

--- src/client/index.js
+++ src/client/index.js
@@ -1,9 +1,9 @@
 import { performAction } from './js/indexscript.js'
-import { updateGui } from './js/updateGui.js'
+
 
 import './styles/style.scss'
 
 export {
     performAction,
-    updateGui,
+
 }
