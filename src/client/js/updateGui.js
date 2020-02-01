c7309f09262e8f81ed92286c10a4b061a013ee64
olkhon <alexander.stapel@gmail.com>
2020-02-01 22:32:58 +0300

    update text msg to tomorrow if selected date tomorrow

--- src/client/js/updateGui.js
+++ src/client/js/updateGui.js
@@ -10,11 +10,21 @@ async function updateGui(data) {
     }
 
     console.log(data);
-    document.getElementById('picDiv').innerHTML = `<img src="${data.imageUrl}" id="imgTagPixabay">`;
-    document.getElementById('textDiv').innerHTML = `<p>Temperature in ${data.city} will be ${data.temperature} Celcius.</p>
-                                                        <p>Your trip to ${data.city} starts in ${data.countdown} days.</p>
-                                                        <p>Your trip will last in total ${data.duration} ${days}  
-                                                        `;
+
+    if (data.countdown == 0) {
+        document.getElementById('picDiv').innerHTML = `<img src="${data.imageUrl}" id="imgTagPixabay">`;
+        document.getElementById('textDiv').innerHTML = `<p>Temperature in ${data.city} will be ${data.temperature} Celcius.</p>
+                                                            <p>Your trip to ${data.city} starts tomorrow.</p>
+                                                            <p>Your trip will last in total ${data.duration} ${days}.</p>  
+                                                            `;
+    } else {
+        document.getElementById('picDiv').innerHTML = `<img src="${data.imageUrl}" id="imgTagPixabay">`;
+        document.getElementById('textDiv').innerHTML = `<p>Temperature in ${data.city} will be ${data.temperature} Celcius.</p>
+                                                            <p>Your trip to ${data.city} starts in ${data.countdown} days.</p>
+                                                            <p>Your trip will last in total ${data.duration} ${days}.</p>  
+                                                            `;
+    }
+
 
 
 }
