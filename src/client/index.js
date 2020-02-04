85fa30512576b14d241a2ff581f45e848687a9eb
olkhon <alexander.stapel@gmail.com>
2020-02-04 23:02:45 +0300

    jquery webpack update

--- src/client/index.js
+++ src/client/index.js
@@ -1,9 +1,21 @@
+require('webpack-jquery-ui');
+
+
+$(function() {
+    $("#arrival").datepicker({
+        minDate: 0,
+
+    });
+    $("#departure").datepicker({
+
+    });
+
+});
+
 import { performAction } from './js/indexscript.js'
-
-
 import './styles/style.scss'
+
 
 export {
     performAction,
-
 }
--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,14 @@ const path = require('path')
 const webpack = require('webpack')
 const HtmlWebPackPlugin = require('html-webpack-plugin')
 
+
+
 module.exports = {
     mode: "development",
     entry: './src/client/index.js',
+    output: {
+        filename: 'index.js',
+    },
     module: {
         rules: [{
                 test: '/\.js$/',
@@ -14,13 +19,23 @@ module.exports = {
             {
                 test: /\.scss$/,
                 use: ['style-loader', 'css-loader', 'sass-loader']
-            }
+            },
+            {
+                test: /\.css$/,
+                loaders: ["style-loader", "css-loader"]
+            },
         ]
     },
     plugins: [
         new HtmlWebPackPlugin({
             template: "./src/client/views/index.html",
             filename: "./index.html"
+        }),
+        new webpack.ProvidePlugin({
+            $: "jquery",
+            jQuery: "jquery",
+            "window.jQuery": "jquery'",
+            "window.$": "jquery"
         })
     ]
 }
