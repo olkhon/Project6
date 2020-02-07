cdfa62a51a5582cfe1ff2221ec0d049340a4785d
olkhon <alexander.stapel@gmail.com>
2020-02-07 14:25:33 +0300

    update countdown

--- src/client/js/updateGui.js
+++ src/client/js/updateGui.js
@@ -1,14 +1,24 @@
 async function updateGui(data) {
-    let days;
+    let countdown;
+    let duration;
 
     if (data.duration > 1) {
-        days = 'days';
+        duration = 'days';
 
     } else {
-        days = 'day';
+        duration = 'day';
     }
+
 
     if (data.countdown == 0) {
+        countdown = "today";
+    } else if (data.countdown == 1) {
+        countdown = "tomorrow";
+    } else {
+        countdown = "days";
+    }
+
+    if (data.countdown <= 1) {
         document.getElementById('holderAnswerApi').style.borderradius = "5px";
         document.getElementById('holderAnswerApi').style.border = "1px solid grey";
 
@@ -16,8 +26,8 @@ async function updateGui(data) {
         document.getElementById('picDiv').innerHTML = `<img src="${data.imageUrl}" id="imgTagPixabay">`;
         document.getElementById('textDiv').innerHTML = `<p>${data.summary}</p>
         <p>Temperature in ${data.city} will be ${data.temperature} Fahrenheit.</p>
-        <p>Your trip to ${data.city} starts tomorrow.</p>
-        <p>Your trip will last in total ${data.duration} ${days}.</p>`;
+        <p>Your trip to ${data.city} starts ${countdown}.</p>
+        <p>Your trip will last in total ${data.duration} ${duration}.</p>`;
     } else {
         document.getElementById('holderAnswerApi').style.borderradius = "5px";
         document.getElementById('holderAnswerApi').style.border = "1px solid grey";
@@ -25,12 +35,9 @@ async function updateGui(data) {
         document.getElementById('picDiv').innerHTML = `<img src="${data.imageUrl}" id="imgTagPixabay">`;
         document.getElementById('textDiv').innerHTML = `<p>${data.summary}</p>
         <p>Temperature in ${data.city} will be ${data.temperature} Fahrenheit.</p>
-        <p>Your trip to ${data.city} starts in ${data.countdown} days.</p>
-        <p>Your trip will last in total ${data.duration} ${days}.</p>`;
+        <p>Your trip to ${data.city} starts in ${data.countdown} ${countdown}.</p>
+        <p>Your trip will last in total ${data.duration} ${duration}.</p>`;
     }
-
-    deleteJourney();
-    printTravelData();
 }
 
 
